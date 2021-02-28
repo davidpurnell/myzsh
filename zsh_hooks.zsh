438afe29ecc360fdbbe051905d13a6b218970dd2
David Purnell <dave@aatom.co>
2021-02-27 19:40:58 -0500

    different terminal title bars depending on whether we're remote or not

--- zsh_hooks.zsh
+++ zsh_hooks.zsh
@@ -1,7 +1,13 @@
 function precmd {
+	if is_remote; then
+    #we're on a remote system
+    echo -ne "\e]0;${USER}@${HOST} $PWD:h:t/$PWD:t\a"
+	else
+    #we're local
+    echo -ne "\e]1;$PWD:h:t/$PWD:t\a"
+	fi
   # vcs_info
   # Put the string "hostname::/full/directory/path" in the title bar:
-  echo -ne "\e]0;${USER}@${HOST}\a"
   #echo -ne "\e]0;$(whoami)@$(hostname):${PWD/#$HOME/~}\a"
 
   # Put the parentdir/currentdir in the tab
